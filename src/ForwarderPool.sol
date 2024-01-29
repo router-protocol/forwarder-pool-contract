eaeada3fbd7c6129245cfea2944c82d691457ced
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2024-01-29 13:06:47 +0530

    feat: basic functions

--- src/ForwarderPool.sol
+++ src/ForwarderPool.sol
@@ -3,10 +3,30 @@ pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IAssetForwarder.sol";
+import "./Multicaller.sol";
 
-contract ForwarderPool is Ownable {
+contract ForwarderPool is Ownable, Multicaller {
     mapping(address => uint256) private erc20Deposits;
     mapping(address => uint256) private nativeTokenDeposits;
+    mapping(address => bool) private whitelistedFillers;
+    IAssetForwarder private assetForwarder;
+
+    address private constant NATIVE_ADDRESS =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    function setAssetForwarder(address _assetForwarder) external onlyOwner {
+        assetForwarder = IAssetForwarder(_assetForwarder);
+    }
+
+    function setWhitelistedFiller(address filler, bool whitelisted) external onlyOwner {
+        whitelistedFillers[filler] = whitelisted;
+    }
+
+    modifier onlyWhitelistedFiller() {
+        require(whitelistedFillers[msg.sender], "ForwarderPool: caller is not whitelisted filler");
+        _;
+    }
 
     function depositERC20(address token, uint256 amount) external {
         IERC20(token).transferFrom(msg.sender, address(this), amount);
@@ -17,9 +37,39 @@ contract ForwarderPool is Ownable {
         nativeTokenDeposits[msg.sender] += msg.value;
     }
 
-    function multicall(address contractAddress, uint256 amount) external onlyOwner {
-        IERC20(contractAddress).approve(contractAddress, amount);
-        (bool success, ) = contractAddress.call(abi.encodeWithSignature("execute()"));
-        require(success, "Multicall failed");
+    function executeIRelay(RelayData memory relayData) external onlyWhitelistedFiller {
+        require(assetForwarder != IAssetForwarder(address(0)), "ForwarderPool: assetForwarder not set");
+        if (isNative(relayData.destToken)) {
+            require(
+                nativeTokenDeposits[address(this)] >= relayData.amount,
+                "ForwarderPool: insufficient native token balance"
+            );
+        } else {
+            require(erc20Deposits[relayData.srcToken] >= relayData.amount, "ForwarderPool: insufficient ERC20 deposit");
+            erc20Deposits[relayData.srcToken] -= relayData.amount;
+            // approve erc20 token to assetForwarder
+            IERC20(relayData.srcToken).approve(address(assetForwarder), relayData.amount);
+        }
+        assetForwarder.execute(relayData);
+    }
+
+    function executeIRelayMessage(RelayMessageData memory relayMessageData) external onlyWhitelistedFiller {
+        require(assetForwarder != IAssetForwarder(address(0)), "ForwarderPool: assetForwarder not set");
+        if (isNative(relayMessageData.destToken)) {
+            require(
+                nativeTokenDeposits[address(this)] >= relayMessageData.amount,
+                "ForwarderPool: insufficient native token balance"
+            );
+        } else {
+            require(erc20Deposits[relayMessageData.srcToken] >= relayMessageData.amount, "ForwarderPool: insufficient ERC20 deposit");
+            erc20Deposits[relayMessageData.srcToken] -= relayMessageData.amount;
+            // approve erc20 token to assetForwarder
+            IERC20(relayMessageData.srcToken).approve(address(assetForwarder), relayMessageData.amount);
+        }
+        assetForwarder.executeMessage(relayMessageData);
+    }
+
+    function isNative(address token) internal pure returns (bool) {
+        return token == NATIVE_ADDRESS;
     }
 }
