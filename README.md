c320b82cad94819463e0b3be1d15f09507a13cdb
Joydeep Singha <67229156+Jds-23@users.noreply.github.com>
2024-02-26 10:22:24 +0400

    docs: deploy docs

--- README.md
+++ README.md
@@ -1,77 +1,94 @@
 # Documentation for ForwarderPool Contract and Test Cases
 ### Overview
-The ForwarderPool contract is a Solidity smart contract designed for Ethereum-based blockchain networks. It facilitates the forwarding of both ERC20 tokens and native blockchain tokens (like Ethereum) to other addresses. This contract also integrates whitelisting functionality to restrict certain actions to authorized addresses only.
+This is ForwarderPool contract facilitates the forwarding of both ERC20 tokens and native blockchain tokens for Router Nitro.
 
 ### Contract Features
-ERC20 and Native Token Management: Handles deposits of ERC20 and native tokens, tracking the balances internally.
-- Asset Forwarding: Uses an IAssetForwarder interface to forward assets.
-- Whitelisting: Provides the ability to whitelist addresses, allowing only authorized addresses to perform certain actions.
-- Owner Privileges: Includes functions that can only be executed by the contract owner, like withdrawing tokens and managing whitelisted addresses.
-Key Functions
-- setAssetForwarder(address _assetForwarder): Sets the address of the asset forwarder. Restricted to the contract owner.
+- **ERC20 and Native Token Management:** Handles deposits of ERC20 and native tokens, tracking the balances internally.
+- **Asset Forwarding:** Uses an IAssetForwarder interface to forward assets.
+- **Whitelisting:** Provides the ability to whitelist addresses, allowing only authorized addresses to forwarder tokens.
+- **Owner Privileges:** Includes functions that can only be executed by the contract owner, like withdrawing tokens and managing whitelisted addresses.
 
-- setWhitelistedFiller(address filler, bool whitelisted): Adds or removes an address from the whitelist. Restricted to the contract owner.
-
-- depositERC20(address token, uint256 amount): Allows users to deposit ERC20 tokens into the contract.
-
-- depositNativeToken(): Allows users to deposit native blockchain tokens (e.g., ETH) into the contract.
-
-- iRelay(IAssetForwarder.RelayData memory relayData): Executes the relay of tokens using the asset forwarder, subject to whitelist and balance checks.
-
-- iRelayMessage(IAssetForwarder.RelayDataMessage memory relayMessageData): Similar to iRelay but for relaying messages.
-
-- withdrawNativeToken(address payable recipient, uint256 amount): Allows the owner to withdraw native tokens from the contract.
-
-- withdrawERC20(address token, address recipient, uint256 amount): Allows the owner to withdraw ERC20 tokens from the contract.
-
-### Test Cases Overview
-The test cases use the Foundry framework, a smart contract development tool for Ethereum. They are designed to validate the functionality of the ForwarderPool contract in various scenarios.
-
-Key Test Functions
-- testSetAssetForwarder(): Tests setting the asset forwarder address.
-
-- testSetWhitelistedFiller(): Tests adding an address to the whitelist.
-
-- testDepositERC20(): Tests depositing ERC20 tokens into the forwarder pool.
-
-- testDepositNativeToken(): Tests depositing native tokens into the forwarder pool.
-
-- testIRelayWithErc20(): Tests the execution of a relay with ERC20 tokens.
-
-- testIRelayWithNativeToken(): Tests the execution of a relay with native tokens.
-
-- testIRelayMessageWithErc20(): Tests the execution of a relay message with ERC20 tokens.
-
-- testIRelayMessageWithNativeToken(): Tests the execution of a relay message with native tokens.
-
-### Test Setup
-Uses Ethereum forks for testing (mumbaiFork).
-Involves mock contracts and users (Interactor, Dummy, DamnValuableToken, etc.).
-Verifies contract behavior under various network conditions and states.
-Conclusion
-The ForwarderPool contract and its tests provide a robust framework for managing and forwarding assets in a decentralized environment, with an emphasis on security through owner privileges and whitelisting. The test cases ensure that the contract functions as expected under different scenarios, contributing to its reliability and trustworthiness.
 
 ## Installation:
 
 Install dependencies using 
+
 ```
 forge install
 ```
 
 ## Compile Contracts:
 
-Compile the smart contracts.
+Compile the smart contracts using the following command:
+
 ```
 forge build
 ```
+
 ## Executing Tests:
 
-Run tests using the command 
+Run tests using the following command:
+ 
 ```
 forge test
 ```
+
 To test specific contracts or scenarios, use: 
+
 ```
 forge test --match-contract <ContractName>
 forge test --match-test <testName>
 ```
+## Deploying Contracts:
+
+Simulate deployment with the following command:
+
+```
+source .env
+forge script script/ForwarderPool.s.sol:ForwarderPoolScript --rpc-url $MUMBAI_RPC -vvv
+```
+
+Deploying the contract and verifying the deployment
+
+```
+source .env
+forge script script/ForwarderPool.s.sol:ForwarderPoolScript --rpc-url $MUMBAI_RPC -vvv --broadcast --verify
+```
+
+If you encounter an error related to EIP1559 fees, add the `--legacy` flag:
+
+```
+Failed to estimate EIP1559 fees. This chain might not support EIP1559, try adding --legacy to your command.
+```
+
+Something like this 👆
+
+```
+source .env
+forge script script/ForwarderPool.s.sol:ForwarderPoolScript --rpc-url $MUMBAI_RPC -vvv --broadcast --verify --legacy
+```
+
+# IMPORTANT: Setting up Router Chain Fee payer and Refund Claimer
+
+Fee Payer: Pays your fees on router chain 
+Refund Claimer: Address who can claim your refund.
+
+### Setting Up Fee payer
+
+The fee payer is responsible for paying your fees on the router chain. Send a request for fee payer by calling  `setDappMetadata(feePayer)` and passing the fee payer's router chain address:
+
+```
+        setDappMetadata(feePayer);
+```
+
+The feePayer will have to approve this request from router chain. [Router Chain Explorer](https://routerscan.io/feePayer)
+
+### Setting Up Refund Claimer
+
+The refund claimer is the address that can claim your refund. Set up the refund claimer by calling `setForwarderRefundClaimer(uint64(expiryTime), refundClaimer, gasBytes)` and passing the refund claimer's router chain address, a expiry and gas in bytes:
+
+```
+        setForwarderRefundClaimer(uint64(expiryTime), refundClaimer, zeroBytes);
+```
+
+
--- src/ForwarderPool.sol
+++ src/ForwarderPool.sol
@@ -4,79 +4,208 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./interfaces/IAssetForwarder.sol";
+import "./interfaces/IGateway.sol";
 // import "./Multicaller.sol";
 
+/**
+ * @title ForwarderPool
+ * @author Router Protocol
+ * @notice Pooling funds to be used by set of whitelisted forwarders 
+ * @custom:version v0.5.0
+ */
 contract ForwarderPool is Ownable {
 
-    constructor() Ownable(msg.sender) {
-
+    /** 
+     * Initializes a new instance of the contract setting up key components necessary for its operation.
+     * This includes setting up the asset forwarder, gateway, and asset forwarder middleware.
+     * The constructor also establishes the contract's owner upon deployment.
+     * 
+     * @param _assetForwarder The asset forwarder interface that handles the forwarding of assets.
+     * @param _gateway The gateway interface to interact with different blockchain protocols.
+     * @param _assetForwarderMiddleware A string representing middleware used in asset forwarding.
+     */
+    constructor(
+        IAssetForwarder _assetForwarder,
+        IGateway _gateway,
+        string memory _assetForwarderMiddleware
+    ) Ownable(msg.sender) {
+        assetForwarder = _assetForwarder;
+        gateway = _gateway;
+        assetForwarderMiddleware = _assetForwarderMiddleware;
     }
 
     mapping(address => uint256) private erc20Deposits;
     mapping(address => uint256) private nativeTokenDeposits;
-    mapping(address => bool) public whitelistedFillers;
+    mapping(address => bool) public whitelistedForwarders;
     IAssetForwarder public assetForwarder;
+    IGateway public gateway;
+    string public assetForwarderMiddleware;
+
+    string private constant destChainId = "router_9625-1"; // this will be depended on router enviroment
 
     address private constant NATIVE_ADDRESS =
         0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
+    /**
+     * Updates the asset forwarder contract address. Only callable by the contract owner.
+     *
+     * @param _assetForwarder The new address of the IAssetForwarder contract.
+     */
     function setAssetForwarder(address _assetForwarder) external onlyOwner {
         assetForwarder = IAssetForwarder(_assetForwarder);
     }
 
-    function setWhitelistedFiller(address filler, bool whitelisted) external onlyOwner {
-        whitelistedFillers[filler] = whitelisted;
+    /**
+     * Updates the gateway contract address. Only callable by the contract owner.
+     *
+     * @param _gateway The new address of the IGateway contract.
+     */
+    function setGateway(address _gateway) external onlyOwner {
+        gateway = IGateway(_gateway);
     }
 
-    modifier onlyWhitelistedFiller() {
-        require(whitelistedFillers[msg.sender], "ForwarderPool: caller is not whitelisted filler");
+    /**
+     * Updates the asset forwarder middleware configuration. Only callable by the contract owner.
+     *
+     * @param _assetForwarderMiddleware The new middleware configuration for the asset forwarder.
+     */
+    function setAssetForwarderMiddleware(string memory _assetForwarderMiddleware) external onlyOwner {
+        assetForwarderMiddleware = _assetForwarderMiddleware;
+    }
+
+    /**
+     * Updates the whitelist status of a forwarder address. Only callable by the contract owner.
+     *
+     * @param forwarder The address of the forwarder to update.
+     * @param whitelisted Boolean indicating whether the forwarder is whitelisted.
+     */
+    function setWhitelistedForwarder(address forwarder, bool whitelisted) external onlyOwner {
+        whitelistedForwarders[forwarder] = whitelisted;
+    }
+
+    /**
+     * Modifier to restrict function access to only whitelisted forwarders.
+     */
+    modifier onlyWhitelistedForwarder() {
+        require(whitelistedForwarders[msg.sender], "ForwarderPool: caller is not whitelisted forwarder");
         _;
     }
 
+
+    /**
+     * Allows a anyone to deposit ERC20.
+     *
+     * @param token The address of the ERC20 token to deposit.
+     * @param amount The amount of tokens to deposit.
+     */
     function depositERC20(address token, uint256 amount) external {
         IERC20(token).transferFrom(msg.sender, address(this), amount);
         erc20Deposits[token] += amount;
     }
 
+    /**
+     * Allows a anyone to deposit native tokens (e.g., ETH).
+     */
     function depositNativeToken() external payable {
         nativeTokenDeposits[msg.sender] += msg.value;
     }
 
-    function iRelay(IAssetForwarder.RelayData memory relayData) external onlyWhitelistedFiller {
+    /**
+     * Sets the refund claimer for the forwarder.
+     * 
+     * @param expiryTime The expiry time for the refund claimer in seconds.
+     * @param refundClaimer The router chain address of the refund claimer.
+     * @param requestMetadata Additional metadata for the request. Like router gas.
+     */
+    function setForwarderRefundClaimer(
+        uint64 expiryTime,
+        string calldata refundClaimer,
+        bytes calldata requestMetadata
+    ) public onlyOwner payable {
+        // Encode the expiry time and refund claimer into a byte array
+        bytes memory packet = abi.encode(expiryTime, refundClaimer);
+
+        // Encode the asset forwarder middleware and the previously created packet into a byte array
+        bytes memory requestPacket = abi.encode(assetForwarderMiddleware, packet);
+
+        // Send the request packet using the gateway contract
+        gateway.iSend{ value: msg.value }(
+          1,
+          0,
+          string(""),
+          destChainId,
+          requestMetadata,
+          requestPacket
+        );
+    }
+
+    /**
+     * Set's the router chain fee payer 
+     * 
+     * @param feePayerAddress The router chain address of the fee payer.
+     */
+    function setDappMetadata(string memory feePayerAddress) external onlyOwner {
+      // Set the Dapp metadata in the gateway contract
+      gateway.setDappMetadata(feePayerAddress);
+    }
+
+    /**
+     * Approves the asset forwarder to spend a certain amount of tokens on behalf of the contract.
+     * 
+     * @param token The address of the token to approve.
+     * @param amount The amount of tokens to approve.
+     */
+    function approveAssetForwarder(address token, uint256 amount) external onlyWhitelistedForwarder {
+        // Approve the asset forwarder to spend the specified amount of tokens
+        IERC20(token).approve(address(assetForwarder), amount);
+    }
+
+    /**
+     * This function relays assets using the provided relay data.
+     * 
+     * @param relayData The data required to perform the relay operation.
+     */
+    function iRelay(IAssetForwarder.RelayData memory relayData) external onlyWhitelistedForwarder {
+        // Ensure that the asset forwarder is set
         require(assetForwarder != IAssetForwarder(address(0)), "ForwarderPool: assetForwarder not set");
+
+        // If the destination token is native (like ETH), call the iRelay function on the assetForwarder
+        // contract with the relay amount as value.
         if (isNative(relayData.destToken)) {
-            // require(
-            //     address(this).balance >= relayData.amount,
-            //     "ForwarderPool: insufficient native token balance"
-            // );
             assetForwarder.iRelay{value: relayData.amount}(relayData);
         } else {
-            // require(erc20Deposits[relayData.destToken] >= relayData.amount, "ForwarderPool: insufficient ERC20 deposit");
-            // erc20Deposits[relayData.destToken] -= relayData.amount;
-            // approve erc20 token to assetForwarder
-            // IERC20(relayData.destToken).approve(address(assetForwarder), relayData.amount);
+            // If the destination token is not native, call the iRelay function on the assetForwarder
+            // contract without sending any value.
             assetForwarder.iRelay(relayData);
         }
     }
 
-    function approveAssetForwarder(address token, uint256 amount) external onlyWhitelistedFiller {
-        IERC20(token).approve(address(assetForwarder), amount);
-    }
-
-    function iRelayMessage(IAssetForwarder.RelayDataMessage memory relayMessageData) external onlyWhitelistedFiller {
+    /**
+     * This function relays a message using the provided relay message data.
+     * 
+     * @param relayMessageData The data required to perform the relay operation.
+     */
+    function iRelayMessage(IAssetForwarder.RelayDataMessage memory relayMessageData) external onlyWhitelistedForwarder {
+        // Ensure that the asset forwarder is set
         require(assetForwarder != IAssetForwarder(address(0)), "ForwarderPool: assetForwarder not set");
+
+        // If the destination token is native (like ETH), check if the contract has enough balance
+        // and then call the iRelayMessage function on the assetForwarder contract with the relay amount as value.
         if (isNative(relayMessageData.destToken)) {
             require(
                 address(this).balance >= relayMessageData.amount,
                 "ForwarderPool: insufficient native token balance"
             );
+            assetForwarder.iRelayMessage{value: relayMessageData.amount}(relayMessageData);
         } else {
+            // If the destination token is not native, check if the contract has enough ERC20 deposits
+            // and then call the iRelayMessage function on the assetForwarder contract without sending any value.
             require(erc20Deposits[relayMessageData.destToken] >= relayMessageData.amount, "ForwarderPool: insufficient ERC20 deposit");
             erc20Deposits[relayMessageData.destToken] -= relayMessageData.amount;
-            // approve erc20 token to assetForwarder
+            // The following line is commented out, but it would approve the asset forwarder to spend the specified amount of ERC20 tokens
             // IERC20(relayMessageData.destToken).approve(address(assetForwarder), relayMessageData.amount);
+            assetForwarder.iRelayMessage(relayMessageData);
         }
-        assetForwarder.iRelayMessage(relayMessageData);
     }
 
     function isNative(address token) internal pure returns (bool) {
